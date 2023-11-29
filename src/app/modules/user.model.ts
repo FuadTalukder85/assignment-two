624708f139d70c9b4dd85302fb899e11d358de69
Fuad Talukder <webfuadtk@gmail.com>
2023-11-29 12:31:48 +0600

    working on validation

--- src/app/modules/user.model.ts
+++ src/app/modules/user.model.ts
@@ -2,33 +2,41 @@ import { Schema, model } from 'mongoose';
 import { Orders, User } from './user/user.interface';
 
 const orderSchema = new Schema<Orders>({
-  productName: { type: String, required: true },
-  price: { type: Number, required: true },
-  quantity: { type: Number, required: true },
+  productName: { type: String, required: [true, 'productName is required'] },
+  price: { type: Number, required: [true, 'price in required'] },
+  quantity: { type: Number, required: [true, 'quantity is required'] },
 });
 
 const userSchema = new Schema<User>({
-  userId: { type: Number, unique: true, required: true },
-  username: { type: String, required: true },
-  password: { type: String, required: true },
+  userId: { type: Number, unique: true, required: [true, 'Id is required'] },
+  username: { type: String, required: [true, 'User name is required'] },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
   fullName: {
     firstName: {
       type: String,
-      required: true,
+      required: [true, 'firstName is required'],
     },
     lastName: {
       type: String,
-      required: true,
+      required: [true, 'lastName is required'],
     },
   },
-  age: { type: Number, required: true },
-  email: { type: String, required: true },
-  isActive: { type: String, enum: ['active', 'blocked'], default: 'active' },
-  hobbies: [{ type: String, required: true }],
+  age: { type: Number, required: [true, 'age is required'] },
+  email: { type: String, required: [true, 'email is required'], unique: true },
+  isActive: {
+    type: String,
+    enum: ['active', 'blocked'],
+    default: 'active',
+    message: 'Invaild status, must be "active" or "blocked" ',
+  },
+  hobbies: [{ type: String, required: [true, 'hobbbies are required'] }],
   address: {
-    street: { type: String, required: true },
-    city: { type: String, required: true },
-    country: { type: String, required: true },
+    street: { type: String, required: [true, 'street is required'] },
+    city: { type: String, required: [true, 'city is required'] },
+    country: { type: String, required: [true, 'country is required'] },
   },
   orders: { type: [orderSchema], required: false },
 });
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
 import { UserService } from './user.service';
+import { userValidationSchema } from './user.zod.validation';
 
 const createUser = async (req: Request, res: Response) => {
   try {
-    const user = req.body.user;
+    // const userValidationSchema = z.object({
+    //   userId: z.number(),
+    //   userName: z.string(),
+    //   password: z.string(),
+    //   fullName: z.object({
+    //     firstName: z.string(),
+    //     lastName: z.string(),
+    //   }),
+    // });
 
-    const result = await UserService.createUserIntoDB(user);
+    const user = req.body.user;
+    const zodValidationSchema = userValidationSchema.parse(user);
+
+    const result = await UserService.createUserIntoDB(zodValidationSchema);
     res.status(200).json({
       success: true,
       message: 'User is created successfully',
