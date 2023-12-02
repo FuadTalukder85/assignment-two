8997e277b661b30945b436a9ae89c17d2ff7a8e0
FuadTalukder85 <webfuadtk@gmail.com>
2023-12-03 00:03:07 +0600

    vercel

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ app.use(cors());
 app.use('/v2', UserRoutes);
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Assignment twoo!');
 });
 
 export default app;
